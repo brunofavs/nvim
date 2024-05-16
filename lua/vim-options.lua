95a89bfc99e8700530c59915afbc1bed8109e936
brunofavs <bruno.favs@ua.pt>
2024-05-16 03:28:29 +0100

    added some very cools keybinds and surround
    i really want a way to save my macros

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,8 @@
 -- Moving half page down/up also centers cursor, to prevent having to search for the cursor
 vim.cmd("noremap <C-d> <C-d>zz ")
 vim.cmd("nnoremap <C-u> <C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 -- Moves to first non whitespace instead of beggining of line
 vim.cmd("map 0 ^")
 vim.cmd("set nu!")
@@ -18,12 +20,22 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.scrolloff = 10
 vim.cmd("let g:python3_host_prog = '/usr/bin/python3'")
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Move selected lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
+-- ººººººººººººººººººººººººººººººººººººººº
+-- 		Auto commands
+-- ººººººººººººººººººººººººººººººººººººººº
 
 local autocmd = vim.api.nvim_create_autocmd
 

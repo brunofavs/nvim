92de93dae09103bf651252b8ca9f19c6004665e4
brunofavs <bruno.favs@ua.pt>
2024-05-25 23:23:12 +0100

    small fix to undotree
    function executing with keymap, thats hella cool

--- lua/plugins/undo-tree.lua
+++ lua/plugins/undo-tree.lua
@@ -1,7 +1,16 @@
 return{
   "mbbill/undotree",
   config = function ()
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+
+    local function openFocusUndoTree()
+      vim.cmd.UndotreeToggle()
+      vim.cmd.UndotreeFocus()
+    end
+
+    -- Doesn't focus by default, that's stupid
+    -- vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    vim.keymap.set('n', '<leader>u', openFocusUndoTree)
 
     vim.opt.swapfile = false
     vim.opt.backup = false

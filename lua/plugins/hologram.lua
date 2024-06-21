8f302c051448059c72d17773e92a4a51c9a3412d
brunofavs <bruno.favs@ua.pt>
2024-06-21 16:44:57 +0100

    fix to lualine

---
+++ lua/plugins/hologram.lua
@@ -0,0 +1,9 @@
+return{
+  -- Not working
+-- "edluffy/hologram.nvim",
+-- config=function ()
+--   require('hologram').setup{
+--     auto_display = true -- WIP automatic markdown image display, may be prone to breaking
+-- }
+-- end
+}

89133a1e46fc9de25f907bdb18cc9361004c0388
brunofavs <bruno.favs@ua.pt>
2024-08-12 15:20:46 +0100

    last commit before stow

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,8 @@
 return {
 	"nvim-lualine/lualine.nvim",
+   dependencies = {
+        "meuter/lualine-so-fancy.nvim",
+    },
 	config = function()
 		local function show_macro_recording()
 			local recording_register = vim.fn.reg_recording()
@@ -9,28 +12,42 @@ return {
 				return "Recording @" .. recording_register
 			end
 		end
+          -- Load the lualine configuration
+          local lualine = require('lualine')
 
-		require("lualine").setup({
-			options = { theme = "dracula" },
-			sections = {
-				lualine_b = { "branch", "diff", "diagnostics",
-            {
-						"macro-recording",
-						fmt = show_macro_recording,
-					  } 
-        },
-				-- lualine_b = {
-				-- 	{
-				-- 		"macro-recording",
-				-- 		fmt = show_macro_recording,
-				-- 	},
-				-- },
-			},
-		})
+          -- Get the default configuration
+          local default_config = lualine.get_config()
+
+          -- Ensure the theme is set to "dracula"
+          default_config.options.theme = "dracula"
+
+          -- Add the new section to lualine_b
+          table.insert(default_config.sections.lualine_b, {
+            "macro-recording",
+            fmt = show_macro_recording,
+          })
+
+          table.insert(default_config.sections.lualine_x, { "fancy_searchcount" })
+
+          -- Setup lualine with the updated configuration
+          lualine.setup(default_config)
+
+    -- This method doens't work to insert new values without reseting the defaults
+		-- require("lualine").setup({
+		-- 	options = { theme = "dracula" },
+		-- 	sections = {
+		-- 		lualine_b = { "branch", "diff", "diagnostics",
+  --           {
+		-- 				"macro-recording",
+		-- 				fmt = show_macro_recording,
+		-- 			  }
+  --       },
+		-- 	},
+		-- })
+		--
 
 		vim.api.nvim_create_autocmd("RecordingEnter", {
 			callback = function()
-				local lualine = require("lualine")
 				lualine.refresh({
 					place = { "statusline" },
 				})
@@ -46,7 +63,6 @@ return {
 				-- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
 				-- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
 				--
-				local lualine = require("lualine")
 				local timer = vim.loop.new_timer()
 				timer:start(
 					50,
